aee46c19954ca7d4471ae2e5fce728c4a97cab37
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-20 21:31:20 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,55 @@
+require 'pry'
+
 class UsersController < ApplicationController
 
-  get '/users/signup' do
-   erb :'/users/signup'
- end
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 
- post '/users' do
-   @user = User.find_by(email: params[:email])
-   session[:user_id] = @user.id
-   redirect '/tweets/tweets'
- end
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to '/tweets'
+    end
+  end
 
- get '/sessions/login' do
-   erb :'sessions/login'
- end
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    end
+  end
 
- post '/sessions' do
-   @user = User.find_by(email: params[:email], password: params[:password])
-   if @user
-     session[:user_id] = @user.id
-     redirect '/tweets/tweets'
-   end
-   redirect '/users/login'
- end
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/tweets'
+    end
+  end
 
- get '/users/logout' do
-   redirect '/'
- end
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect to '/signup'
+    end
+  end
 
-
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
