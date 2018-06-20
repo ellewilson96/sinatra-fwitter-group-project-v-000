e700a82955a7867bc49533de69b0f6d09eca5713
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-20 21:27:23 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
    @user = User.find_by(email: params[:email], password: params[:password])
    if @user
      session[:user_id] = @user.id
-     redirect '/users/home'
+     redirect '/'
    end
    redirect '/sessions/login'
  end
