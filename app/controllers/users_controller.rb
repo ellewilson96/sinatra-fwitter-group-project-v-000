31467d079fd6d1a09a8fae2c25e4608cdb3e9173
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-20 21:37:06 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/login'
     else
-      redirect to '/tweets'
+      redirect to '/tweets/tweets'
     end
   end
 
