e4e9d12cb2961871b89e3e0f28d82423f84664bb
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-20 20:53:40 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,4 +11,16 @@ class TweetsController < ApplicationController
 
   post '/tweets' do
   end
+
+  get '/tweets/:id' do
+    erb :show_tweet
+  end
+
+  get '/tweets/:id/edit' do
+    erb :edit_tweet
+  end
+
+  post '/tweets/:id' do
+  end
+  
 end
