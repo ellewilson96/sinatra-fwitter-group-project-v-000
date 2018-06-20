3d58065ca08dad74720ca7b171f96de459c3dc7d
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-20 20:51:51 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,11 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+    erb :
   end
   
   get '/tweets/new' do
-    @tweet = Tweet.new(params)
+    @tweet = Tweet.new(params[:id])
   end
 
   post '/tweets' do
